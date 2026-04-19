f3afaf246b4176b86436eb27545887821d484008
Alfa <danilinaM@gmail.com>
2026-04-19 13:23:59 +0300

    code for lesson2

---
+++ src/main/java/alfa/Lesson_2.java
@@ -0,0 +1,21 @@
+package alfa;
+
+import java.util.Random;
+
+public class Lesson_2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int age = random.nextInt(0, 99);
+        double sum = random.nextDouble(100000);
+        boolean hasInvitation = false;
+        boolean inBlackList = false;
+        boolean reachMinAge = (age >= 18);
+        boolean hasEnoughMoneyOrInvite = (sum > 50000) || hasInvitation;
+        boolean canVisitMeeting = reachMinAge && hasEnoughMoneyOrInvite && (!inBlackList);
+        double fee = sum * 0.075;
+        System.out.println(
+                        "Проход разрешен: " + canVisitMeeting + "\n" +
+                        "Обязательный добровольный взнос: " + fee
+        );
+    }
+}
