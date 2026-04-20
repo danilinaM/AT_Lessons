ab674242d1e03bda269e92af5957bc783f7399f9
Alfa <danilinaM@gmail.com>
2026-04-20 17:06:01 +0300

    remarks after review

--- src/main/java/alfa/Lesson_2.java
+++ src/main/java/alfa/Lesson_2.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class Lesson_2 {
     public static void main(String[] args) {
         Random random = new Random();
-        int age = random.nextInt(0, 99);
+        int age = random.nextInt( 99);
         double sum = random.nextDouble(100000);
         boolean hasInvitation = false;
         boolean inBlackList = false;
-        boolean reachMinAge = (age >= 18);
-        boolean hasEnoughMoneyOrInvite = (sum > 50000) || hasInvitation;
+        boolean reachMinAge = age >= 18;
+        boolean hasEnoughMoneyOrInvite = sum > 50000 || hasInvitation;
         boolean canVisitMeeting = reachMinAge && hasEnoughMoneyOrInvite && (!inBlackList);
         double fee = sum * 0.075;
         System.out.println(
