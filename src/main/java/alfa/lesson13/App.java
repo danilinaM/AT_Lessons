207350ecc9f26999489c24ec578928d09423b925
Liudmila Danilina <ldanilina@alfabank.ru>
2026-07-14 09:35:17 +0300

    added names of checks in main class

--- src/main/java/alfa/lesson13/App.java
+++ src/main/java/alfa/lesson13/App.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class App {
 
     public static void main(String[] args) {
+
+        //Часть 1: База данных пришельцев
         ArrayList<Alien> arrAlien = new ArrayList<>();
         arrAlien.addAll(List.of(
                 new Alien("Shrek", "Mars", 4),
@@ -15,12 +17,15 @@ public class App {
         ));
         checkDuplicate(arrAlien);
 
+        //Часть 2: Формирование отрядов
         SquadManager squadManager = new SquadManager();
         squadManager.demonstareteListCreations();
 
+        //Часть 3: Отсеивание трусов
         ArrayList<String> cowards = new ArrayList<>(List.of("Петя", "Вася", "Иван", "Трус-Петя", "Трус Ваня"));
         squadManager.filterOutCowards(cowards);
 
+        //Часть 4: Очередь на вход
         LinkedList<String> linkedList = new LinkedList<>(List.of("one", "two", "three", "four", "five"));
         AssaultQueue assaultQueue = new AssaultQueue(linkedList);
         assaultQueue.retreatCoward();
@@ -30,6 +35,7 @@ public class App {
         assaultQueue.addRecruit("eight");
         assaultQueue.printQueue();
 
+        //Часть 5: Отчёт командованию
         MissionReport missionReport = new MissionReport("Миссия разведка", arrAlien, 55);
         System.out.println(missionReport.toString());
         MissionReport m2 = new MissionReport("Миссия разведка", arrAlien, 5);
@@ -37,6 +43,7 @@ public class App {
         System.out.println("Сравнение через 'equals' : " + (m2.equals(missionReport)));
     }
 
+    //Часть 1: База данных пришельцев
     private static void checkDuplicate(ArrayList<Alien> arr) {
         Set<Alien> set = new HashSet();
         for (Alien alien : arr) {
